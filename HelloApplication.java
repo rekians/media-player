280cca38f85a60162148d54844a55518ce456ea0
NikitaBarmin <114222379+NikitaBarmin@users.noreply.github.com>
2023-06-17 14:02:13 +0300

    add hello aplication final version file

--- HelloApplication.java
+++ HelloApplication.java
@@ -4,8 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.util.Objects;
 
 
 public class HelloApplication extends Application {
@@ -15,8 +15,7 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 1000, 500);
         stage.setScene(scene);
         stage.show();
-        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
         Controller controller = fxmlLoader.getController();
         controller.setIcons();
     }
